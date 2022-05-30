d686b98edf1fe69f5309f12d8e20c695ab14ead0
Richard Liu <102811722+bjRichardLiu@users.noreply.github.com>
2022-05-30 17:51:17 -0400

    Add files via upload

---
+++ FourPartHarmony/src/chordList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class chordList {
+    private ArrayList<Chord> chords;
+
+    public chordList(ArrayList<Chord> chords) {
+        this.chords = chords;
+    }
+
+    public ArrayList<Chord> getChords() {
+        return chords;
+    }
+
+    public void setChords(ArrayList<Chord> chords) {
+        this.chords = chords;
+    }
+}
