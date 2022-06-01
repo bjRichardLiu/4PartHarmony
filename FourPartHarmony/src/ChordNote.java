e48b4c985a3c84f5cb240cbd0fb727cb0dcb1ac0
Richard Liu <102811722+bjRichardLiu@users.noreply.github.com>
2022-05-31 23:32:12 -0400

    Update ChordNote.java

--- FourPartHarmony/src/ChordNote.java
+++ FourPartHarmony/src/ChordNote.java
@@ -10,15 +10,30 @@ public class ChordNote {
         this.base = base;
     }
 
-    public ArrayList<Integer> getNoteNum(Scale scale) {
+    public Integer[] getNoteNum(Scale scale) {
+        //todo difference between integer and int
 
         notesInChords = new int[3];
         notesInChords[0] = scale.getAvailableNotesInScale()[base];
         notesInChords[1] = scale.getAvailableNotesInScale()[base + 2];
         notesInChords[2] = scale.getAvailableNotesInScale()[base + 4];
 
+        for (int i = 0; i < 3; i++) {
+            if (notesInChords[i] >= 12) {
+                notesInChords[i] -= 12;
+            }
+        }
 
+        Integer[] notesToUse;
+        ArrayList<Integer> noteToUseArrayList = new ArrayList<>();
+        //from E2(52) to C6(96)
+        for (int p = 52; p < 96; p++) {
+            if (p % 12 == notesInChords[0] || p % 12 == notesInChords[1] || p % 12 == notesInChords[2]) {
+                noteToUseArrayList.add(p);
+            }
+        }
 
+        notesToUse = noteToUseArrayList.toArray(new Integer[0]);
 
         return notesToUse;
     }
