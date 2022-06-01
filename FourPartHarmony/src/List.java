d94cb89e5b502a9c5e2771b4167e6e07b08b81b4
Richard Liu <102811722+bjRichardLiu@users.noreply.github.com>
2022-05-31 23:33:06 -0400

    Update List.java

--- FourPartHarmony/src/List.java
+++ FourPartHarmony/src/List.java
@@ -1,17 +1,17 @@
 import java.util.ArrayList;
 
 public class List {
-    private ArrayList<chordList> chordArrayList;
+    private ArrayList<ChordList> chordArrayList;
 
-    public List(ArrayList<chordList> chordArrayList) {
+    public List(ArrayList<ChordList> chordArrayList) {
         this.chordArrayList = chordArrayList;
     }
 
-    public ArrayList<chordList> getChordArrayList() {
+    public ArrayList<ChordList> getChordArrayList() {
         return chordArrayList;
     }
 
-    public void setChordArrayList(ArrayList<chordList> chordArrayList) {
+    public void setChordArrayList(ArrayList<ChordList> chordArrayList) {
         this.chordArrayList = chordArrayList;
     }
 }
